4767e8c4d1894ef67e3c7be5171f7633ad76e48f
朱正钱 <money.zhu@zwoptical.com>
2024-12-01 21:17:54 +0800

    test loop print time

--- st7735_led/lib/screen/screen.h
+++ st7735_led/lib/screen/screen.h
@@ -11,6 +11,12 @@ const uint16_t numberColor = 0x6dcb;
 const uint16_t defaultScreenColor =  0x2965;
 const uint8_t defaultTextSize = 1;
 
+const uint8_t numberLeftPix = 42;
+const uint8_t leftPix = 5;
+const uint8_t topPix = 5;
+const uint8_t titleNumberMargin = 5;
+const uint8_t blockMargin = 10;
+
 #define TFT_CS   5   // 芯片选择 (D1)
 #define TFT_RST  4   // 重置 (D2)
 #define TFT_DC   0   // 数据/命令 (D3)
@@ -22,6 +28,47 @@ void initTft(){
     tft.setRotation(1);       // 设置屏幕方向
     tft.fillScreen(defaultScreenColor);
     tft.setTextSize(defaultTextSize);
+}
+
+void drawImageAndNumber(int s30Number , int s50Number, int imageNumber){
+    tft.drawRGBBitmap(leftPix, leftPix, epd_bitmap_seestar_logo_2_ezgif, 30, 30); 
+    tft.drawRGBBitmap(topPix, 90, epd_bitmap_logo_fddb51a7_ezgif, 30, 30); 
+
+  // 1 size = 8 ,2 size = 16
+
+    tft.setTextColor(numberColor);
+    tft.setTextSize(1);
+    tft.setCursor(numberLeftPix, topPix);
+    tft.print("S30");
+
+    // 18 + 16 + 10 = 44
+    tft.setCursor(numberLeftPix, topPix + 8 + titleNumberMargin + 16 + blockMargin);
+    tft.print("S50");
+
+    tft.setTextColor(defaultTextColor);
+    // 5+8+5
+    tft.setCursor(numberLeftPix, topPix + 8 + titleNumberMargin);
+    tft.setTextSize(2);
+    tft.print(s30Number);
+
+    // 44 +8 + 5 = 57
+    tft.setCursor(numberLeftPix, topPix + 8 + titleNumberMargin + 16 + blockMargin + 8 + titleNumberMargin);
+    tft.setTextSize(2);
+    tft.print(s50Number);
+
+    // 绘制横线
+    int x1 = numberLeftPix, y1 = 85, x2 = 128-5, y2 = 85;
+    tft.drawLine(x1, y1, x2, y2, ST7735_WHITE); // 绘制白色横线
+
+    // 90 + 1 + 
+    tft.setCursor(numberLeftPix, 90 + 5);
+    tft.setTextColor(numberColor);
+    tft.setTextSize(1);
+    tft.print("Image");
+
+    tft.setCursor(numberLeftPix, 110);
+    tft.setTextColor(defaultTextColor);
+    tft.print(imageNumber);
 }
 
 void fullScreenPrint(const char c[], uint8_t textSize){
--- st7735_led/src/main.cpp
+++ st7735_led/src/main.cpp
@@ -7,11 +7,6 @@
 #include <screen.h>
 #include <SPI.h>
 
-const uint8_t numberLeftPix = 42;
-const uint8_t leftPix = 5;
-const uint8_t topPix = 5;
-const uint8_t titleNumberMargin = 5;
-const uint8_t blockMargin = 10;
 
 void setup() {
     Serial.begin(115200);
@@ -19,50 +14,32 @@ void setup() {
     // 连接到 WiFi
     // connectToWiFi();
 
-    tft.drawRGBBitmap(leftPix, leftPix, epd_bitmap_seestar_logo_2_ezgif, 30, 30); 
-    tft.drawRGBBitmap(topPix, 90, epd_bitmap_logo_fddb51a7_ezgif, 30, 30); 
-
-  // 1 size = 8 ,2 size = 16
-
-    tft.setTextColor(numberColor);
-    tft.setTextSize(1);
-    tft.setCursor(numberLeftPix, topPix);
-    tft.print("S30");
-
-    // 18 + 16 + 10 = 44
-    tft.setCursor(numberLeftPix, topPix + 8 + titleNumberMargin + 16 + blockMargin);
-    tft.print("S50");
-
-    tft.setTextColor(defaultTextColor);
-    // 5+8+5
-    tft.setCursor(numberLeftPix, topPix + 8 + titleNumberMargin);
-    tft.setTextSize(2);
-    tft.print("178888");
-
-    // 44 +8 + 5 = 57
-    tft.setCursor(numberLeftPix, topPix + 8 + titleNumberMargin + 16 + blockMargin + 8 + titleNumberMargin);
-    tft.setTextSize(2);
-    tft.print("98765");
-
-    // 绘制横线
-    int x1 = numberLeftPix, y1 = 90, x2 = 128-5, y2 = 90;
-    tft.drawLine(x1, y1, x2, y2, ST7735_WHITE); // 绘制白色横线
-
-    tft.setCursor(numberLeftPix, 100);
-    tft.setTextSize(1);
-    tft.print("998765");
-    
+    drawImageAndNumber(millis(),millis(), millis());
 }
-
-
 
 // 上次执行 Wi-Fi 请求的时间
 unsigned long lastRequestTime = 0;
 unsigned long requestInterval = 6000*5;  // 每 1 分钟请求一次 API
 
-bool isFirst = true;
-
 void loop() {
+  unsigned long currentMillis = millis();
+   // 每 1 分钟请求一次 API
+  if (currentMillis - lastRequestTime >= requestInterval) {
+    // 记录当前时间
+    lastRequestTime = currentMillis;
+    tft.fillScreen(defaultScreenColor);
+    drawImageAndNumber(millis(),millis(), millis());
+  }
+
+  for (int frame = 0; frame < FRAME_COUNT ; frame++) {
+      tft.drawRGBBitmap(128-50, 128-50, frames[frame], 50, 50); // 假设图像大小为 128x128
+      delay(100); // 控制帧率
+  }
+    // 延迟1s
+  delay(1000);
+}
+
+void loop2() {
       for (int frame = 0; frame < FRAME_COUNT ; frame++) {
         tft.drawRGBBitmap(128-50, 128-50, frames[frame], 50, 50); // 假设图像大小为 128x128
         delay(100); // 控制帧率
@@ -71,7 +48,7 @@ void loop() {
     delay(1000);
 }
 
-void loop2() {
+void loop3() {
     if (isFirst){
         tft.setCursor(1,30);
         tft.setTextSize(2);
